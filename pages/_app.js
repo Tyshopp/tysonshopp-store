4cd55e9fe7e6ed8c617f9c067b3e4ccb3f55f9ca
Tyshopp <germanleon867@gmail.com>
2025-05-14 17:50:15 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // Forzando redeploy para que Vercel actualice los imports
-import '../globals.css';
+import '@/globals.css';
+
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
